1cc2c991703cdac6c1855865caf6ff308a640c44
Evandro2270 <evandro535@gmail.com>
2024-12-06 09:36:10 -0300

    Adicionar arquivos de projeto.

---
+++ ExerciciosCondicional4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                CultureInfo CI = CultureInfo.InvariantCulture;
+
+                string[] vet = Console.ReadLine().Split(' ');
+                int horaInicial = int.Parse(vet[0]);
+                int horaFinal = int.Parse(vet[1]);
+
+                int duracao;
+
+                if (horaInicial < horaFinal)
+                {
+                    duracao = horaFinal - horaInicial; 
+                }
+                else
+                {
+                    duracao = 24 - horaInicial + horaFinal;
+                }
+                Console.WriteLine("O jogo durou " + duracao + " Hora(s)");
+
+            }
+        }
+    }
+}
